cd2d97020a98070368512dc671f50825ae6b9fc1
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-07-04 02:47:30 +0200

    refactored main.py (#1)

--- homework-1/main.py
+++ homework-1/main.py
@@ -5,9 +5,35 @@ import os
 from secret_data import PASSWORD
 
 
-def build_path(file):
+def build_path(file: str) -> str:
+    """
+    Возвращает путь от корня проекта к указанному файлу в папке 'north_data'.
+
+    file: название файла
+    """
+
     path_to_file = os.path.join(os.path.dirname(__file__), 'north_data', file)
     return path_to_file
+
+
+def add_data_to_table(table: str, data: pandas.DataFrame, cursor: psycopg2.extensions.cursor):
+    """
+    Добавляет переданные данные в указанную таблицу базы данных.
+
+    table: название таблицы в базе данных
+    data: данные для записи в таблицу
+    cursor: объект для взаимодействия с базой данных
+    """
+
+    cursor.execute(f"TRUNCATE TABLE {table} CASCADE")
+
+    columns = data.columns.tolist()
+    string_placeholders = ", ".join(["%s"] * len(columns))
+    command = f"INSERT INTO {table} VALUES ({string_placeholders})"
+
+    for index, row in data.iterrows():
+        values = [row[column] for column in columns]
+        cursor.execute(command, tuple(values))
 
 
 if __name__ == '__main__':
@@ -23,36 +49,9 @@ if __name__ == '__main__':
             password=PASSWORD
     ) as conn:
         with conn.cursor() as cur:
-            cur.execute("TRUNCATE TABLE orders CASCADE")
-            cur.execute("TRUNCATE TABLE customers CASCADE")
-            cur.execute("TRUNCATE TABLE employees CASCADE")
 
-            for index, row in customers.iterrows():
-                insert = "INSERT INTO customers VALUES (%s, %s, %s)"
-                values = (row['customer_id'], row['company_name'], row['contact_name'])
-                cur.execute(insert, values)
-
-            for index, row in employees.iterrows():
-                insert = "INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)"
-                values = (
-                    row['employee_id'],
-                    row['first_name'],
-                    row['last_name'],
-                    row['title'],
-                    row['birth_date'],
-                    row['notes']
-                )
-                cur.execute(insert, values)
-
-            for index, row in orders.iterrows():
-                insert = "INSERT INTO orders VALUES (%s, %s, %s, %s, %s)"
-                values = (
-                    row['order_id'],
-                    row['customer_id'],
-                    row['employee_id'],
-                    row['order_date'],
-                    row['ship_city']
-                )
-                cur.execute(insert, values)
+            add_data_to_table("customers", customers, cur)
+            add_data_to_table("employees", employees, cur)
+            add_data_to_table("orders", orders, cur)
 
     conn.close()
